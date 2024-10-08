40b3c7f431804bab6c038a95a200b74477ddf331
rajesh kumar <rajeshit22003@gmail.com>
2024-10-08 14:34:57 +0530

    final submit

--- program1.py
+++ program1.py
@@ -4,7 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []  
+        bracket_map = {')': '(', '}': '{', ']': '['} 
+
+        
+        for char in s:
+            if char in bracket_map:
+              
+                
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                
+                stack.append(char)
+
+        
+        return not stack
+
 
 
 
--- program2.py
+++ program2.py
@@ -4,7 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
+        romanMap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        for i in range(len(s)):
+            currentVal = romanMap[s[i]]
+            if i + 1 < len(s) and currentVal < romanMap[s[i + 1]]:
+                total -= currentVal
+            else:
+                total += currentVal
+        return total
 
